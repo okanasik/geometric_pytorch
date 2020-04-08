90a5e5b6438e90c42bb3776bd6b21b19c9ca0766
Okan Asik <asik.okan@gmail.com>
2020-04-08 16:37:02 +0300

    first test with graph classification using topk_model

--- rescue/train.py
+++ rescue/train.py
@@ -3,17 +3,18 @@ import torch.nn.functional as F
 from torch_geometric.data.dataloader import DataLoader
 from rescue_dataset import RescueDataset
 from gcn_model import GCNNet
+from topk_model import TopKNet
 
-dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", "robocup2019", "test", "ait")
-train_loader = DataLoader(dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(dataset, batch_size=1, shuffle=False)
+node_classification = False
+dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019", scenario="test",
+                        team="ait", node_classification=node_classification)
+train_loader = DataLoader(dataset, batch_size=128, shuffle=True)
+test_loader = DataLoader(dataset, batch_size=128, shuffle=False)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = GCNNet(dataset.num_features, dataset.num_classes).to(device)
-optimizer = torch.optim.Adam([
-    dict(params=model.reg_params, weight_decay=5e-4),
-    dict(params=model.non_reg_params, weight_decay=0)
-], lr=0.01)
+# model = GCNNet(dataset.num_features, dataset.num_classes).to(device)
+model = TopKNet(dataset.num_features, dataset.num_classes).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
 
 
 def train():
@@ -36,17 +37,12 @@ def test():
     for data in test_loader:
         data = data.to(device)
         output = model(data)
-        pred = output.max(1)[1]
-        # print("pred:{}".format(pred))
-        # print("true:{}".format(data.y))
-        y_index = torch.argmax(data.y)
-        # print("pred:{} true:{}".format(pred, y_index))
-        pred_index = torch.argmax(pred)
-        correct += int(y_index == pred_index)
-    return float(correct) / len(dataset)
+        pred = output.max(dim=1)[1]
+        correct += pred.eq(data.y).sum().item()
+    return correct / len(dataset)
 
 
-for epoch in range(1, 201):
+for epoch in range(1, 501):
     loss = train()
     accuracy = test()
     log = 'Epoch: {:03d}, Train Loss: {:.4f} Accuracy: {:.4f}'
