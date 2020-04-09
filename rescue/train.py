b1d992bbe1f4c4227f9023b4ce28f9de4d104b38
Okan Asik <asik.okan@gmail.com>
2020-04-09 17:23:01 +0300

    model saving is added for topk model. we are able overfit upto 0.80 test2 dataset using topk using upto 4000 epoch with manually decreasing learning rates

--- rescue/train.py
+++ rescue/train.py
@@ -6,24 +6,33 @@ from topk_model import TopKNet
 from gcn_model import GCNNet
 from agnn_model import AGNNNet
 
-node_classification = True
-dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019", scenario="Kobe1",
+# parameters
+node_classification = False
+batch_size = 64
+
+dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019", scenario="test3",
                         team="ait", node_classification=node_classification)
-train_loader = DataLoader(dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+if node_classification:
+    test_loader = DataLoader(dataset, batch_size=1, shuffle=False)
+else:
+    test_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # model = GCNNet(dataset.num_features, dataset.num_classes).to(device)
 # optimizer = torch.optim.Adam([
 #     dict(params=model.reg_params, weight_decay=5e-4),
 #     dict(params=model.non_reg_params, weight_decay=0)
-# ], lr=0.001)
+# ], lr=0.01)
 
-# model = TopKNet(dataset.num_features, dataset.num_classes).to(device)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
+model = TopKNet(dataset.num_features, dataset.num_classes)
+# model.load_state_dict(torch.load('./topk_model_test4.pth'))
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
-model = AGNNNet(dataset.num_features, dataset.num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+# model = AGNNNet(dataset.num_features, dataset.num_classes).to(device)
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
 def train():
     model.train()
@@ -56,8 +65,14 @@ def test():
     return correct / len(dataset)
 
 
-for epoch in range(1, 501):
+for epoch in range(1, 1001):
     loss = train()
     accuracy = test()
     log = 'Epoch: {:03d}, Train Loss: {:.8f} Accuracy: {:.8f}'
     print(log.format(epoch, loss, accuracy))
+
+# accuracy = test()
+# log = 'Epoch: {:03d}, Train Loss: {:.8f} Accuracy: {:.8f}'
+# print(log.format(0, 0, accuracy))
+
+# torch.save(model.state_dict(), './topk_model_test4.pth')
