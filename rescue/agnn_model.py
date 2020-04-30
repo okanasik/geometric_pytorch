f1a9f431b95706ba549bc136e7def6594426a95b
Okan Asik <asik.okan@gmail.com>
2020-04-30 18:14:22 +0300

    experimented whether a simple linear model can learn the buildings that are on fire fieryness between 1 and 3. the linear model was quite unsuccessfull until we add batch normalization. when batch normalization is added after 3-5 epoch it reach 100% accuracy

--- rescue/agnn_model.py
+++ rescue/agnn_model.py
@@ -6,16 +6,20 @@ from torch_geometric.nn import AGNNConv
 class AGNNNet(torch.nn.Module):
     def __init__(self, num_features, num_classes):
         super(AGNNNet, self).__init__()
-        self.lin1 = torch.nn.Linear(num_features, 64)
-        self.conv1 = AGNNConv(requires_grad=True)
-        self.conv2 = AGNNConv(requires_grad=True)
-        self.lin2 = torch.nn.Linear(64, num_classes)
+        self.bn1 = torch.nn.BatchNorm1d(num_features=num_features)
+        self.lin1 = torch.nn.Linear(num_features, 32)
+        self.bn2 = torch.nn.BatchNorm1d(num_features=32)
+        # self.conv1 = AGNNConv(requires_grad=True)
+        # self.conv2 = AGNNConv(requires_grad=True)
+        self.lin2 = torch.nn.Linear(32, num_classes)
 
     def forward(self, data):
-        x = F.dropout(data.x, training=self.training)
-        x = F.relu(self.lin1(x))
-        x = self.conv1(x, data.edge_index)
-        x = self.conv2(x, data.edge_index)
-        x = F.dropout(x, training=self.training)
+        x = data.x
+        x = self.bn1(x)
+        # x = F.dropout(x, training=self.training)
+        x = F.relu(self.bn2(self.lin1(x)))
+        # x = self.conv1(x, data.edge_index)
+        # x = self.conv2(x, data.edge_index)
+        # x = F.dropout(x, training=self.training)
         x = self.lin2(x)
-        return F.log_softmax(x, dim=1)
+        return x
---
+++ rescue/train_fieryness.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.data.dataloader import DataLoader
+from rescue_dataset import RescueDataset
+from topk_model import TopKNet
+from gcn_model import GCNNet
+from agnn_model import AGNNNet
+from soft_assignment_loss import soft_assignment_loss
+
+# parameters
+node_classification = True
+batch_size = 1
+
+dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019", scenario="test2",
+                        team="ait", node_classification=node_classification)
+
+train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# model = GCNNet(dataset.num_features, dataset.num_classes)
+model = AGNNNet(dataset.num_features, dataset.num_classes).to(device)
+
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+
+
+def get_fieryness_target(fieryness_values, size, device):
+    nodes_with_fieryness = [idx for idx, fieryness in enumerate(fieryness_values) if
+                            fieryness >= 1 and fieryness <= 3]
+    num_fieryness = len(nodes_with_fieryness)
+    target = torch.zeros(size, device=device, dtype=torch.long)
+    target[nodes_with_fieryness] = 1
+    return target, num_fieryness
+
+def train():
+    model.train()
+    total_loss = 0
+    for data in train_loader:
+        optimizer.zero_grad()
+        data = data.to(device)
+        output = model(data)
+        target, num_fieryness = get_fieryness_target(data.x[:, 5].view(-1), data.y.size(), device)
+        class_weight = torch.tensor([num_fieryness/data.num_nodes, (data.num_nodes-num_fieryness)/data.num_nodes], dtype=torch.float32, device=device)
+        loss = F.cross_entropy(output, target, weight=class_weight)
+        # loss = F.cross_entropy(output, target)
+        loss.backward()
+        total_loss += loss.item()
+        optimizer.step()
+    return total_loss / len(dataset)
+
+
+def test():
+    model.eval()
+    correct = 0
+    total = 0
+    total_fieryness = 0
+    for data in test_loader:
+        data = data.to(device)
+        output = model(data)
+        pred = output.max(dim=1)[1]
+        target, num_fieryness = get_fieryness_target(data.x[:, 5].view(-1), data.y.size(), device)
+        this_correct = pred.eq(target).sum().item()
+        correct += this_correct
+        total += data.num_nodes
+        total_fieryness += num_fieryness
+    return correct / total, total_fieryness / total
+
+
+for epoch in range(1, 1001):
+    loss = train()
+    accuracy, fieryness_ratio = test()
+    log = 'Epoch: {:03d}, Train Loss: {:.8f} Train Accuracy: {:.8f} Fieryness Ratio: {}'
+    print(log.format(epoch, loss, accuracy, fieryness_ratio))
+
+# accuracy = test()
+# log = 'Epoch: {:03d}, Train Loss: {:.8f} Accuracy: {:.8f}'
+# print(log.format(0, 0, accuracy))
+
+# torch.save(model.state_dict(), './topk_model_test4.pth')
