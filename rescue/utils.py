c2350b0b15a4d99b066fed0c3e39f279188ad128
Okan Asik <asik.okan@gmail.com>
2020-05-09 15:18:37 +0300

    a util method is added to read gml files to extract buildings and roads. a util method is added to count the number of agents in a given xml scenario file

--- rescue/utils.py
+++ rescue/utils.py
@@ -26,6 +26,7 @@
 # sum_indexes[0] = zero_count
 # print(sum_indexes)
 from datetime import datetime
+import xml.etree.ElementTree as et
 
 
 def convert_to_datetime(datetime_str):
@@ -43,5 +44,40 @@ def convert_to_datetime(datetime_str):
                     minute=datetime_values[4], second=datetime_values[5])
 
 
-file_name = 'robocup2019_Kobe1_ait_ambulanceteam_280282693_20200422140450.zip'
-print(file_name[-3:])
+def read_rescue_gml(file_path):
+    tree = et.parse(file_path)
+    root = tree.getroot()
+
+    building_ids = []
+    for building in root.iter('{urn:roborescue:map:gml}building'):
+        bid = building.attrib['{http://www.opengis.net/gml}id']
+        building_ids.append(bid)
+
+    road_ids = []
+    for road in root.iter('{urn:roborescue:map:gml}road'):
+        rid = road.attrib['{http://www.opengis.net/gml}id']
+        road_ids.append(rid)
+
+    return building_ids, road_ids
+
+
+def read_num_agents(scn_file_path):
+    tree = et.parse(scn_file_path)
+    root = tree.getroot()
+    count = 0
+    for amb in root.iter('{urn:roborescue:map:scenario}ambulanceteam'):
+        count += 1
+    for fb in root.iter('{urn:roborescue:map:scenario}firebrigade'):
+        count += 1
+    for police in root.iter('{urn:roborescue:map:scenario}policeforce'):
+        count += 1
+
+    return count
+
+
+if __name__ == '__main__':
+    # bids, rids = read_rescue_gml('/home/okan/rescuesim/scenarios/robocup2019/test2/map/map.gml')
+    # print(bids)
+    # print(rids)
+
+    print(read_num_agents('/home/okan/rescuesim/scenarios/robocup2019/test2/map/scenario.xml'))
