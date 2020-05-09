b7eca93df3689a3245eb57a0cf2ba80a01479b1a
Okan Asik <asik.okan@gmail.com>
2020-05-09 15:17:09 +0300

    new dataset class for directly loading and saving single file datasets and keeping all data in the main memory

---
+++ rescue/inmemory_rescue_dataset.py
@@ -0,0 +1,37 @@
+from torch_geometric.data import Dataset
+import torch
+
+
+class InMemoryRescueDataset(Dataset):
+    def __init__(self, data_list, node_classification=False):
+        self.data_list = data_list
+        self.node_classification = node_classification
+
+        self.pre_transform = None
+        self.transform = None
+        self.__indices__ = None
+
+    def get(self, idx):
+        if idx < 0 or idx > self.len():
+            raise IndexError()
+
+        return self.data_list[idx]
+
+    def len(self):
+        return len(self.data_list)
+
+    @property
+    def num_classes(self):
+        r"""The number of classes in the dataset."""
+        # whether node is selected as target or not
+        if self.node_classification:
+            return 2
+        else:
+            data = self.get(0)
+            return data.x.size(0)
+
+    def save(self, file_name):
+        torch.save(self.data_list, file_name)
+
+    def load(self, file_name):
+        self.data_list = torch.load(file_name)
