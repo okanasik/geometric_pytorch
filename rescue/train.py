5cdecbce2ff42ecb8425c7521f6ba8f17f7d424d
Okan Asik <asik.okan@gmail.com>
2020-05-09 15:23:50 +0300

    encapsulate all main functions under main to be able to import this module from other python modules. add a new method to read train and test datasets

--- rescue/train.py
+++ rescue/train.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn.functional as F
 from torch_geometric.data.dataloader import DataLoader
 from rescue_dataset import RescueDataset
+from inmemory_rescue_dataset import InMemoryRescueDataset
+from rescue_dataset_list import RescueDatasetList
 from topk_model import TopKNet
 from gcn_model import GCNNet
 from agnn_model import AGNNNet
@@ -9,36 +11,39 @@ from soft_assignment_loss import soft_assignment_loss
 
 # parameters
 node_classification = False
-batch_size = 64
-
-dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019", scenario="test2",
-                        team="ait", node_classification=node_classification)
-
-train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-if node_classification:
-    test_loader = DataLoader(dataset, batch_size=1, shuffle=False)
-else:
-    test_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+batch_size = 256
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model = GCNNet(dataset.num_features, dataset.num_classes)
-# optimizer = torch.optim.Adam([
-#     dict(params=model.reg_params, weight_decay=5e-4),
-#     dict(params=model.non_reg_params, weight_decay=0)
-# ], lr=0.01)
+def get_datasets():
+    # train dataset
+    train_datasets = []
+    for i in range(50,300):
+        dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019",
+                                scenario="test"+str(i),
+                                team="ait", node_classification=node_classification, read_info_map=True)
+        train_datasets.append(dataset)
+    train_dataset_list = RescueDatasetList(train_datasets)
 
-model = TopKNet(dataset.num_features, dataset.num_classes)
-# model.load_state_dict(torch.load('./topk_model_test4.pth'))
-model = model.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 
-# model = AGNNNet(dataset.num_features, dataset.num_classes).to(device)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    # test dataset
+    test_datasets = []
+    for i in range(300, 400):
+        dataset = RescueDataset("/home/okan/rescuesim/rcrs-server/dataset", "firebrigade", comp="robocup2019",
+                                scenario="test" + str(i),
+                                team="ait", node_classification=node_classification, read_info_map=True)
+        test_datasets.append(dataset)
+    test_dataset_list = RescueDatasetList(test_datasets)
+
+    print('Train dataset len:{}'.format(len(train_dataset_list)))
+    print('Test dataset len:{}'.format(len(test_dataset_list)))
+
+    return train_dataset_list, test_dataset_list
+
 
 def train():
     model.train()
     total_loss = 0
+    total_graph = 0
     for data in train_loader:
         optimizer.zero_grad()
         data = data.to(device)
@@ -52,13 +57,15 @@ def train():
         loss.backward()
         total_loss += loss.item()
         optimizer.step()
-    return total_loss / len(dataset)
+        total_graph += data.num_graphs
+    return total_loss / total_graph
 
 
-def test():
+def test_model(data_loader):
     model.eval()
     correct = 0
-    for data in test_loader:
+    total_graph = 0
+    for data in data_loader:
         data = data.to(device)
         output = model(data)
         if node_classification:
@@ -69,14 +76,50 @@ def test():
         else:
             pred = output.max(dim=1)[1]
             correct += pred.eq(data.y).sum().item()
-    return correct / len(dataset)
+        total_graph += data.num_graphs
+    return correct / total_graph
 
 
-for epoch in range(1, 1001):
-    loss = train()
-    accuracy = test()
-    log = 'Epoch: {:03d}, Train Loss: {:.8f} Train Accuracy: {:.8f}'
-    print(log.format(epoch, loss, accuracy))
+if __name__ == '__main__':
+    # import analyze_dataset
+    # train_dataset, test_dataset = get_datasets()
+    # train_dataset, test_dataset = analyze_dataset.get_notrandom_notnull(train_dataset, test_dataset,
+    #                                                                     node_classification=node_classification)
+    train_dataset = InMemoryRescueDataset([], node_classification=node_classification)
+    train_dataset.load('train_dataset.pt')
+
+    test_dataset = InMemoryRescueDataset([], node_classification=node_classification)
+    test_dataset.load('test_dataset.pt')
+    print(len(train_dataset))
+    print(len(test_dataset))
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    if node_classification:
+        testtrain_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
+    else:
+        testtrain_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # model = GCNNet(dataset.num_features, dataset.num_classes)
+    model = TopKNet(train_dataset.num_features, train_dataset.num_classes)
+    # model.load_state_dict(torch.load('./topk_model_test4.pth'))
+    # model = AGNNNet(dataset.num_features, dataset.num_classes)
+
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+    for epoch in range(1, 1001):
+        loss = train()
+        train_accuracy = test_model(testtrain_loader)
+        test_accuracy = test_model(test_loader)
+        log = 'Epoch: {:03d}, Train Loss: {:.8f} Train Accuracy: {:.8f} Test Accuracy: {:.8f}'
+        print(log.format(epoch, loss, train_accuracy, test_accuracy))
+        if epoch % 10 == 0:
+            print("Saving model topk")
+            torch.save(model.state_dict(), './topk_test_gat.pth')
+            print('Model: ' +  './topk_test_gat.pth' + ' is saved.')
 
 # accuracy = test()
 # log = 'Epoch: {:03d}, Train Loss: {:.8f} Accuracy: {:.8f}'
